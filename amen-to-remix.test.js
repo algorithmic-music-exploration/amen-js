342bd1381cbb3225bda3bc68d3f7f400e6c2ba6b
Thor Kell <thor@tide-pool.ca>
2017-07-23 18:12:22 -0400

    working dummy test

---
+++ amen-to-remix.test.js
@@ -0,0 +1,12 @@
+amenToRemix = require('./amen-to-remix');
+
+// test('convert a track to a jremix-style object', () => {
+       //  var track = setupTrack;
+       //  var result = amenToRemix.amenToRemix(track)
+//       expect(result.segements[0].confidence).toBe(0.0);
+// etc!
+// });
+
+test('Dummy test to make sure that jest is working', () => {
+    expect(true).toBe(true);
+});
