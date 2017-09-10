1a1e52ead8efa145ecab003c21ac03bf1876124b
Thor Kell <thor@tide-pool.ca>
2017-09-10 16:32:38 -0400

    testing array playback, refactor setup for tests

--- tests/amen-player.test.js
+++ tests/amen-player.test.js
@@ -1,19 +1,11 @@
 require('web-audio-test-api');
 amenPlayer = require('../amen-player');
 
-var context = new AudioContext();
-var player = amenPlayer.amenPlayer(context, null);
+var context;
+var player;
 
-test('amen player dummy test', () => {
-    expect(true).toEqual(true);
-});
-
-test('amenPlayer checks the current time', () => {
-    res = (context.currentTime === player.curTime());
-    expect(res).toEqual(true);
-});
-
-test('amenPlayer plays audio and advances time', () => {
+function setupBuffers(context) {
+    // Mock the buffer creation process so we can follow what happened
     var injectedBufferSrc = context.createBufferSource();
     var mockCreateBufferSource = jest.fn();
     mockCreateBufferSource.mockReturnValue(injectedBufferSrc);
@@ -21,8 +13,43 @@ test('amenPlayer plays audio and advances time', () => {
 
     // create a half-second buffer at 44100
     var buffer = context.createBuffer(2, 22050, 44100);
-    res = player.play(0, buffer);
 
+    return {
+            'buffer': buffer,
+            'injectedBufferSrc': injectedBufferSrc
+           }
+}
+
+beforeEach(() => {
+    context = new AudioContext();
+    player = amenPlayer.amenPlayer(context, null);
+});
+
+test('amenPlayer checks the current time', () => {
+    res = (context.currentTime === player.curTime());
+    expect(res).toEqual(true);
+});
+
+test('amenPlayer plays from a buffer and advances time', () => {
+    res = setupBuffers(context)
+    var buffer = res.buffer;
+    var injectedBufferSrc = res.injectedBufferSrc
+    res = player.play(0, buffer);
+    expect(res).toEqual(0.5);
+
+    context.$processTo('00:00.000');
+    expect(injectedBufferSrc.$state).toEqual('PLAYING');
+    context.$processTo('00:00.501');
+    expect(injectedBufferSrc.$state).toEqual('FINISHED');
+});
+
+test('amenPlayer plays from an array and advances time', () => {
+    res = setupBuffers(context)
+    var buffer = res.buffer;
+    var injectedBufferSrc = res.injectedBufferSrc
+    var playbackArray = [buffer]
+
+    res = player.play(0, playbackArray);
     expect(res).toEqual(0.5);
 
     context.$processTo('00:00.000');
